9dfdb5a6072596786a9f6c091b379a5fd1999ceb
Josiah Elias <josiahelias11@gmail.com>
2024-03-27 03:09:17 +0100

    feat: set up material tailwind and other tools for the frontend development of the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      orange: "#e64a19",
+    },
+  },
+  plugins: [],
+});
